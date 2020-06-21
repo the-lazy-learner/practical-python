484ade4484a823397065319d7f393bb6a4570821
Sudhnsu Bhushan Mishra <sbmishra2000@gmail.com>
2020-06-21 20:08:01 +0530

    Finish Ex 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # in metres
+times_bounced = 0
+
+while times_bounced <= 10:
+    times_bounced = times_bounced + 1
+    height = 3 / 5 * height
+    print(times_bounced, round(height, 4))
